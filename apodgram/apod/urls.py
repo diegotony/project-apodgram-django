ab33d1645be72a4e17828fba7596fe09b764c02a
Diego <tucotony1396@gmail.com>
2020-04-25 00:36:37 -0500

    added views of image

--- apodgram/apod/urls.py
+++ apodgram/apod/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import  format_suffix_patterns
 from apod import views
 
 urlpatterns = [
     path('apod/authors/', views.author_list),
-    path('apod/authors/<int:pk>/', views.author_detail),
+    path('apod/author/<int:pk>/', views.author_detail),
+    path('apod/images/', views.image_list),
+    path('apod/image/<int:pk>/', views.image_detail),
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
