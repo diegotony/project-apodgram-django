ca3988cb0f6614d2c5f1bc20156578043ff52f7e
Diego <tucotony1396@gmail.com>
2020-04-29 15:56:21 -0500

    models updated

--- apod/models.py
+++ apod/models.py
@@ -12,15 +12,15 @@ class Author(models.Model):
 
 
 class Image(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='images')
+    copyright = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='images')
     # owner = models.ForeignKey('auth.User', related_name='images', on_delete=models.CASCADE)
-    date_photo = models.DateTimeField()
+    date = models.DateTimeField()
     data_save = models.DateTimeField(auto_now=True)
     explanation = models.TextField()
-    hd_url_image = models.TextField()
-    url_image = models.TextField()
+    hdurl = models.TextField()
+    url = models.TextField()
     media_type = models.TextField()
-    version = models.CharField(max_length=10)
+    service_version = models.CharField(max_length=10)
     title = models.TextField()
 
     def __str__(self):
